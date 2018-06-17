710a1e2565acefcc5eecba1928500311533d2ec1
Qingyang Hu <huqingyang2000@gmail.com>
2018-06-17 16:14:31 +0800

    Basic login

--- app.js
+++ app.js
@@ -1,46 +1,33 @@
 //app.js
 App({
     onLaunch: function () {
-        // 展示本地存储能力
-        let logs = wx.getStorageSync('logs') || []
-        logs.unshift(Date.now())
-        wx.setStorageSync('logs', logs)
-        wx.request({
-          url: 'https://nfls.io/user/csrf',
-          success: function(res) {
-            console.log(res.data)
-          }
-        })
-        // 登录
-        wx.login({
-            success: res => {
-                console.log(res.code)
-                // 发送 res.code 到后台换取 openId, sessionKey, unionId
-            }
-        })
-        // 获取用户信息
-        wx.getSetting({
-            success: res => {
-                if (res.authSetting['scope.userInfo']) {
-                    // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-                    wx.getUserInfo({
-                        success: res => {
-                            // 可以将 res 发送给后台解码出 unionId
-                            console.log(res.encryptedData)
-                            this.globalData.userInfo = res.userInfo
-
-                            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-                            // 所以此处加入 callback 以防止这种情况
-                            if (this.userInfoReadyCallback) {
-                                this.userInfoReadyCallback(res)
-                            }
-                        }
-                    })
-                }
-            }
-        })
     },
     globalData: {
         userInfo: null
+    },
+    requestAPI: function(path, data, method, success) {
+        var cookie = ""
+        let value = wx.getStorageSync('PHPSESSID')
+        if (value) {
+            cookie = "PHPSESSID="+value+";"
+        } else {
+            cookie = ""
+        }
+
+        wx.request({
+            url: "https://nfls.io/" + path,
+            data: data,
+            method: method,
+            header: {
+                "Cookie" : cookie
+            },
+            success: function (res) {
+                if(res.header["Set-Cookie"]) {
+                    var cookie = res.header["Set-Cookie"].split(";")[0].split("=")[1]
+                    wx.setStorageSync("PHPSESSID", cookie)
+                }
+                success(data)
+            }
+        })
     }
 })
